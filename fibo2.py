21ffd716e628cd52b7605e59f06b74c1a32ce003
Bindiya2024 <126401522+Bindiya2024@users.noreply.github.com>
2023-04-10 00:04:39 +0530

    Add files via upload

---
+++ fibo2.py
@@ -0,0 +1,11 @@
+def fib(n):
+    """ Calculates the n-th Fibonacci number iteratively """
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+
+if fib(0) == 0 and fib(10) == 55 and fib(50) == 12586269025:
+    print("Test for the fib function was successful!")
+else:
+    print("The fib function is returning wrong values!")
